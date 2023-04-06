3efe64a2e22e3252e67e188ee272a4a8a4598027
sanoop78 <sanoop7887@gmail.com>
2023-04-06 23:57:09 +0000

    second change

--- test.java
+++ test.java
@@ -1,5 +1,5 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Java from feature2");  
+     System.out.println("Hello Java from feature2 to feature1");  
     }  
 }  
